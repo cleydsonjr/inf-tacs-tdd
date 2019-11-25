abf6532116a3f18a6c946552c7c7801a7993b5e7
danielmachado98 <danielmachado0411@gmail.com>
2019-11-25 20:41:35 -0300

    Adiciona testes da classe Leilao

--- src/main/java/br/com/caelum/leilao/dominio/Leilao.java
+++ src/main/java/br/com/caelum/leilao/dominio/Leilao.java
@@ -6,26 +6,24 @@ import java.util.List;
 
 public class Leilao {
 
-	private String descricao;
-	private List<Lance> lances;
-	
-	public Leilao(String descricao) {
-		this.descricao = descricao;
-		this.lances = new ArrayList<Lance>();
-	}
-	
-	public void propoe(Lance lance) {
-		lances.add(lance);
-	}
+    private String descricao;
+    private List<Lance> lances;
 
-	public String getDescricao() {
-		return descricao;
-	}
+    public Leilao(String descricao) {
+        this.descricao = descricao;
+        this.lances = new ArrayList<Lance>();
+    }
 
-	public List<Lance> getLances() {
-		return Collections.unmodifiableList(lances);
-	}
+    public void propoe(Lance lance) {
+        lances.add(lance);
+    }
 
-	
-	
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public List<Lance> getLances() {
+        return Collections.unmodifiableList(lances);
+    }
+
 }
